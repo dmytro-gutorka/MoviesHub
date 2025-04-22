d27a524f0107ff6849a2c6494a59f1c9a04fe9ac
dimagutorka <dgutorka@gmail.com>
2025-04-22 18:17:38 +0300

    Add styles for modal window for burger button

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -26,30 +26,32 @@ const Header = (props) => {
         {
             label: 'Subscriptions',
             href: '/subscriptions',
-        }
+        },
     ]
 
     return (
         <header className="header">
             <div className="header__inner container">
                 <Logo className="header__logo" loading="eager" />
-                <nav className="header__menu">
-                    <ul className="header__menu-list">
-                        {menuItems.map(({label, href}, index) => (
-                            <li className="header__menu-item" key={index}>
-                                <a className={classNames("header__menu-link", {
-                                    'is-active': href === url})} href={href}>
-                                    {label}
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-                <div className="header__actions">
-                    <Button className="header__button" label="Search" isLabelHidden iconName="search" mode="transparent"/>
-                    <Button className="header__button" label="Notifications" isLabelHidden iconName="notification" mode="transparent"/>
-                </div>
-                <BurgerButton className="header__burger-button"/>
+                <dialog className="header__overlay-menu-dialog">
+                    <nav className="header__menu">
+                        <ul className="header__menu-list">
+                            {menuItems.map(({label, href}, index) => (
+                                <li className="header__menu-item" key={index}>
+                                    <a className={classNames("header__menu-link", {
+                                        'is-active': href === url})} href={href}>
+                                        {label}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                    <div className="header__actions">
+                        <Button className="header__button" label="Search" isLabelHidden iconName="search" mode="transparent"/>
+                        <Button className="header__button" label="Notifications" isLabelHidden iconName="notification" mode="transparent"/>
+                    </div>
+                </dialog>
+                <BurgerButton className="header__burger-button visible-tablet"/>
             </div>
         </header>
     )
