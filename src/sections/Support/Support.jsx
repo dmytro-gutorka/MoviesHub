bff533d7a80ca3a723e6e204de364a711283cac4
dimagutorka <dgutorka@gmail.com>
2025-04-30 22:57:54 +0300

    Create Form & Field components

---
+++ src/sections/Support/Support.jsx
@@ -0,0 +1,61 @@
+import './Support.scss'
+import classNames from 'classnames'
+import {Image} from "minista";
+import Field from "@/components/Field";
+
+const Support = () => {
+    const titleId = 'support-title';
+
+    return (
+        <section className="support container" aria-labelledby={titleId}>
+            <div className="support__body">
+                <div className="support__info" id={titleId}>
+                    <h1 className="support_title h2">Welcome to our support page!</h1>
+                </div>
+                <div className="support__description">
+                    <p>
+                        We're here to help you with any problems you may be having with our product.
+                    </p>
+                </div>
+                <Image className="support__image" src="/src/assets/images/support/1.png"/>
+            </div>
+            <div>
+                <form className="support__form" action="">
+                    <Field
+                        className="support__form-cell"
+                        label="First Name"
+                        placeholder="John"
+                        isRequired
+                    />
+                    <Field
+                        className="support__form-cell"
+                        label="Last Name"
+                        placeholder="Libosky"
+                    />
+                    <Field
+                        className="support__form-cell"
+                        label="Email"
+                        placeholder="example@example.com"
+                        isRequired
+                    />
+                    <Field
+                        className="support__form-cell"
+                        label="Phone Number"
+                        placeholder="(111) 222-33-44"
+                        inputMode="tel"
+                        mask="(000) 000-00-00"
+                    />
+                    <Field
+                        className="support__form-cell support__form-cell--wide"
+                        label="Message"
+                        type="textarea"
+                        placeholder="Hi! I have a question..."
+                        isRequired
+                    />
+                </form>
+            </div>
+        </section>
+    )
+}
+
+export default Support
